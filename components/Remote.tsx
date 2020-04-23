1d29c583af7a86642208926aee4fb3717c01fcf1
Peter PJT <pjtsignups@gmail.com>
2020-04-22 21:37:27 -0700

    feat(Remote.tsx): add remote-lib
    Add remote-lib, add example in Remote.tsx

--- components/Remote.tsx
+++ components/Remote.tsx
@@ -1,8 +1,74 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Col, Row, Grid } from "react-native-easy-grid";
+import { Device,HttpAction,Macro,Remote,HLayout } from "remote-lib";
 
-export default function Remote() {
+
+export default function CompRemote() {
+    (async ()=>{
+        const TVBase = "http://10.0.2.105:1234"
+        const TV = new Device({name:"TV",actions:[
+            new HttpAction({
+                name:"on",
+                method:"GET",
+                base:TVBase,
+                path:"/tvon"
+            }),
+            new HttpAction({
+                name:"off",
+                method:"GET",
+                base:TVBase,
+                path:"/tvoff"
+            }),
+        ]})
+
+        const STBBase = "http://10.0.2.105:1234"
+        const STB = new Device({name:"STB",actions:[
+            new HttpAction({
+                name:"on",
+                method:"GET",
+                base:STBBase,
+                path:"/stbon"
+            }),
+            new HttpAction({
+                name:"off",
+                method:"GET",
+                base:STBBase,
+                path:"/stboff"
+            }),
+        ]})
+
+        const fios = new Remote({name:"Fios",layout:[
+            {
+                left:null,
+                center:null,
+                right:new HLayout([
+                    new Macro({name:"on",actions:[
+                        TV.getAction("on"),
+                        STB.getAction("on")
+                    ]})
+                ])
+            },
+            {
+                left:null,
+                center:null,
+                right:null
+            },
+            {
+                left:null,
+                center:null,
+                right:null
+            },
+            {
+                left:null,
+                center:null,
+                right:null
+            }
+        ]})
+
+        console.log(fios)
+    })()
+
     return (
     <View style={styles.container}>
         <Grid>
