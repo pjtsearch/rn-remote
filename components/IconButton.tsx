9cf4b448478aa934d7c046269d927e2d09a97579
Peter PJT <pjtsignups@gmail.com>
2020-04-25 12:35:09 -0700

    feat(IconButton): change border color on IconButton click
    Make IconButton change border color on click for clarity

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View,Dimensions, Vibration } from 'react-native';
 import { VLayout } from 'remote-lib';
 import { Button } from 'react-native-paper';
@@ -8,8 +8,13 @@ import { scale, vscale } from '../utils/scale';
 
 export default function IconButton({icon,onPress, style}:{icon?:string,onPress:Function,style?:any}) {
     const height = 60 * vscale
+    const [borderColor,$borderColor] = useState("#999")
+    const press = ()=>{
+        $borderColor("#448aff")
+        setTimeout(()=>$borderColor("#999"),200)
+    }
     return (
-        <Button style={{...RemoteButton.button,...style}} contentStyle={{minHeight:height,marginLeft:-32,transform:[{scale:2}]}} labelStyle={{display:"none"}} icon={icon} mode="outlined" onPress={()=>{onPress();Vibration.vibrate(10)}}>{null}</Button>
+        <Button style={{...RemoteButton.button,...style,borderColor}} contentStyle={{minHeight:height,marginLeft:-32,transform:[{scale:2}]}} labelStyle={{display:"none"}} icon={icon} mode="outlined" onPress={()=>{onPress();press();Vibration.vibrate(10)}}>{null}</Button>
     );
 }
 
