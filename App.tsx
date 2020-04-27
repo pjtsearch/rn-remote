b6d28c2f9a72a590c3f2ad42ee820aab1684c894
Peter PJT <pjtsignups@gmail.com>
2020-04-27 10:47:53 -0700

    refactor(App): start switching to react native reanimated

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@ import { Provider as PaperProvider, DefaultTheme, IconButton, Surface, Button } 
 import { vscale, scale } from './utils/scale';
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
-import * as Animatable from 'react-native-animatable';
+import Animated, { Easing } from 'react-native-reanimated';
+
+const { Value, timing,concat } = Animated;
 import { house } from './House';
 import { nookTV, centralMatrix } from './devices';
 import { centralAudioReceiver } from './devices/audioReceiver';
@@ -33,38 +35,86 @@ export default function App() {
     'Google Sans Medium': require('./assets/fonts/GoogleSansMedium.ttf'),
     'Google Sans': require('./assets/fonts/GoogleSans.ttf'),
   });
-  var [menuView,$menuView]:any = useState({});
-  var [menuTitle,$menuTitle]:any = useState({});
-  var [menuButton,$menuButton]:any = useState({});
-  var [surfaceTitle,$surfaceTitle]:any = useState({});
-  var [surfaceView,$surfaceView]:any = useState({});
+  // var [menuView,$menuView]:any = useState({});
+  // var [menuTitle,$menuTitle]:any = useState({});
+  // var [menuButton,$menuButton]:any = useState({});
+  // var [surfaceTitle,$surfaceTitle]:any = useState({});
+  // var [surfaceView,$surfaceView]:any = useState({});
   const menuItems = house.rooms.flat().map(room=>room.remotes).flat()
   // const handleMenuViewRef = (ref:any) => menuView = ref;
   // const handleSurfaceViewRef = (ref:any) => surfaceView = ref;
   // const handleSurfaceTitleRef = (ref:any) => {surfaceTitle = ref;console.log(!surfaceTitle);}
+  const [menuHeight] = useState(new Value(0))
+  const [remoteOpacity] = useState(new Value(1))
+  const [surfaceTitleOpacity] = useState(new Value(0))
+  const [surfaceTitleHeight] = useState(new Value(0))
 
   let [menuOpen,$menuOpen] = useState(false)
   const toggleMenu = () => {
     if (!menuOpen){
-      menuView.animate({ 0: { height: "0%" }, 1: { height: "81%"} });
-      surfaceView.animate({0: { opacity:1 }, 1: { opacity:0 } })
+      // // menuView.animate({ 0: { height: "0%" }, 1: { height: "81%"} });
+      // // surfaceView.animate({0: { opacity:1 }, 1: { opacity:0 } })
+      // // setTimeout(()=>{
+      // //   surfaceTitle.animate({0: { opacity:0,height:0}, 1: { opacity:1,height:30} })
+      // // },100)
+      // menuTitle.animate({0: { opacity:1 }, 1: { opacity:0 } })
+      // menuButton.animate({0: { opacity:1 }, 1: { opacity:0 } })
+      // $menuOpen(true)
+      // menuTitle.animate({0: { opacity:0 }, 1: { opacity:1 } })
+      // menuButton.animate({0: { opacity:0 }, 1: { opacity:1 } })
+      timing(menuHeight, {
+        duration: 400,
+        toValue: 81,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      }).start()
+      timing(remoteOpacity, {
+        duration: 400,
+        toValue: 0,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      }).start()
       setTimeout(()=>{
-        surfaceTitle.animate({0: { opacity:0,height:0}, 1: { opacity:1,height:30} })
+        timing(surfaceTitleOpacity, {
+          duration: 400,
+          toValue: 1,
+          easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+        }).start()
+        timing(surfaceTitleHeight, {
+          duration: 400,
+          toValue: 30,
+          easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+        }).start()
       },100)
-      menuTitle.animate({0: { opacity:1 }, 1: { opacity:0 } })
-      menuButton.animate({0: { opacity:1 }, 1: { opacity:0 } })
       $menuOpen(true)
-      menuTitle.animate({0: { opacity:0 }, 1: { opacity:1 } })
-      menuButton.animate({0: { opacity:0 }, 1: { opacity:1 } })
     }else {
-      surfaceTitle.animate({0: { opacity:1,height:30}, 0.5: { opacity:0, height:0}, 1:{opacity:0, height:0} })
-      menuView.animate({ 0: { height: "81%" }, 1: { height: "0%"} });
-      surfaceView.animate({0: { opacity:0 }, 1: { opacity:1 } })
-      menuTitle.animate({0: { opacity:0 }, 1: { opacity:1 } })
-      menuButton.animate({0: { opacity:1 }, 1: { opacity:0 } })
+      // // surfaceTitle.animate({0: { opacity:1,height:30}, 0.5: { opacity:0, height:0}, 1:{opacity:0, height:0} })
+      // // menuView.animate({ 0: { height: "81%" }, 1: { height: "0%"} });
+      // // surfaceView.animate({0: { opacity:0 }, 1: { opacity:1 } })
+      // menuTitle.animate({0: { opacity:0 }, 1: { opacity:1 } })
+      // menuButton.animate({0: { opacity:1 }, 1: { opacity:0 } })
+      // $menuOpen(false)
+      // menuTitle.animate({0: { opacity:0 }, 1: { opacity:1 } })
+      // menuButton.animate({0: { opacity:0 }, 1: { opacity:1 } })
+      timing(surfaceTitleOpacity, {
+        duration: 200,
+        toValue: 0,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      }).start()
+      timing(surfaceTitleHeight, {
+        duration: 200,
+        toValue: 0,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      }).start()
+      timing(menuHeight, {
+        duration: 400,
+        toValue: 0,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      }).start()
+      timing(remoteOpacity, {
+        duration: 400,
+        toValue: 1,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      }).start()
       $menuOpen(false)
-      menuTitle.animate({0: { opacity:0 }, 1: { opacity:1 } })
-      menuButton.animate({0: { opacity:0 }, 1: { opacity:1 } })
     }
   }
   if (!fontsLoaded) {
@@ -74,23 +124,26 @@ export default function App() {
       <PaperProvider theme={theme}>
         <View style={styles.container}>
           <View style={{display:"flex", alignItems:"stretch", justifyContent:"space-between",flexDirection:"row"}}>
-            <Animatable.View duration={200} ref={$menuButton}>
+            <Animated.View>
               <IconButton icon={menuOpen ? "arrow-left":"menu"} color="white" onPress={()=>toggleMenu()}></IconButton>
-            </Animatable.View>
-            <Animatable.Text duration={200} style={styles.title} ref={$menuTitle}>{menuOpen ? "Menu" : remote.name}</Animatable.Text>
+            </Animated.View>
+            <Animated.Text style={styles.title}>{menuOpen ? "Menu" : remote.name}</Animated.Text>
             <IconButton icon="dots-vertical" color="white"></IconButton>
           </View>
-          <Animatable.View style={{height:0,overflow:"hidden"}} ref={$menuView} duration={500}>
+          <Animated.View style={{height:concat(menuHeight.interpolate({
+          inputRange: [0, 100],
+          outputRange: [0, 100],
+        }),"%"),overflow:"hidden"}}>
             {menuItems.map((item,i)=>
               <Button mode={"outlined"} color="white" style={{margin:10,borderWidth:2,backgroundColor:item == remote?"#ffffff77":"transparent"}} onPress={()=>{$remote(item);toggleMenu()}} key={i}>{item.name}</Button>
             )}
-          </Animatable.View>
+          </Animated.View>
           <TouchableWithoutFeedback onPress={()=>{if(menuOpen)toggleMenu()}}>
             <Surface style={styles.surface}>
-              <Animatable.Text style={{...styles.title,opacity:0,height:0}} ref={$surfaceTitle}>{remote.name}</Animatable.Text>
-              <Animatable.View ref={$surfaceView} duration={500}>
+              <Animated.Text style={{...styles.title,opacity:surfaceTitleOpacity,height:surfaceTitleHeight}}>{remote.name}</Animated.Text>
+              <Animated.View style={{opacity:remoteOpacity}}>
                 <Remote remote={remote}/>
-              </Animatable.View>
+              </Animated.View>
             </Surface>
           </TouchableWithoutFeedback>
         </View>
